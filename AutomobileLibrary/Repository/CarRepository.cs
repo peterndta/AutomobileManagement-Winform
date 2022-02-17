71952e1f9f31edf66bef52ad12826c6ccbb401bb
TruongAnh <truonganh20062001@gmail.com>
2022-02-17 21:28:22 +0700

    Upload file

---
+++ AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,33 @@
+﻿using AutomobileLibrary.BussinessObject;
+using AutomobileLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public IEnumerable<Car> GetCars() => CarDBContext.Instance.GetCarList;
+
+        public Car GetCarById(int carId) => CarDBContext.Instance.GetCarByID(carId);
+
+        public void InsertCar(Car car)
+        {
+            CarDBContext.Instance.AddNew(car);
+        }
+
+        public void DeleteCar(int carId)
+        {
+            CarDBContext.Instance.Remove(carId);
+        }
+
+        public void UpdateCar(Car car)
+        {
+            CarDBContext.Instance.Update(car);
+        }
+
+    }
+}
